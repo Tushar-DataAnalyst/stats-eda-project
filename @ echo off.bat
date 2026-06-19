21cc954f1ea6dd6e522a7951639744729996f11e
Tushar Kishor <tushar.kishore.arya@gmail.com>
2026-06-19 15:27:56 +0530

    Update EDA -Boxplot Outlier Analysis

---
+++ @ echo off.bat
@@ -0,0 +1,12 @@
+@ echo off
+
+echo =========================
+echo Activate jupyter notebook 
+echo =========================
+
+cd /d D:\Stats
+call statsenv\Scripts\activate
+jupyter notebook
+
+pause 
+	
